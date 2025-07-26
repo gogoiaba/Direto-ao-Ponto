37555f2393f0a9e5d48073499dc0b298d707666a
Igor Sá <167645991+gogoiaba@users.noreply.github.com>
2025-07-26 17:49:55 -0300

    tirei a funcionalidade de excluir com o x do lado da celula

--- script.js
+++ script.js
@@ -18,16 +18,6 @@ document.getElementById("adicionarHorario").addEventListener("click", () => {
     </label>
   `;
 
-  if (contadorLinhas > 2) {
-    const btnRemover = document.createElement("button");
-    btnRemover.type = "button";
-    btnRemover.textContent = "x";
-    btnRemover.addEventListener("click", () => {
-      novaLinha.remove();
-    });
-    novaLinha.appendChild(btnRemover);
-  }
-
   document.getElementById("horarios").appendChild(novaLinha);
 });
 
